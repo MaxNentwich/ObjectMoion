23a63582a3e1a7d2a4f25798134b35c6fd51a20c
MaxNentwich <max.nentwich@gmail.com>
2020-08-03 15:02:22 -0400

    Initializing variables and paths

---
+++ setup_object_motion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 09:57:07 2020
+
+@author: max
+"""
+
+###
+# Setup paths to data and variables frequently used
+###
+
+# Import libraries
+import sys
+
+# Define directories
+annot_dir = '/home/max/Documents/Dropbox (City College)/Annotation'
+present_dir = 'present_all_frames_annot_copy'
+
+# Add directories to path 
+sys.path.append(annot_dir)
